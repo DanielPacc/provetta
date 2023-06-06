8e8d9d1ccc2684235246be5ecfc3a24125691384
Trabyy <daniele@pacciani.net>
2023-06-06 18:02:53 +0000

    suvvia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, NavBarComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule], // Aggiungi FormsModule ai moduli importati
   providers: [],
   bootstrap: [AppComponent]
--- src/connect.service.ts
+++ src/connect.service.ts
@@ -10,16 +10,16 @@ export class connect {
   constructor(private http: HttpClient) { }
 
   getTableData(): Observable<any[]> {
-    return this.http.get<any[]>('/');
+    return this.http.get<any[]>('/api/GR.php');
   }
 
   getTableDatafl(filt: string): Observable<any[]> {
-    return this.http.post<any[]>('/', filt);
+    return this.http.post<any[]>('', filt);
   }
 
 
   insertData(data: any): Observable<any> {
-    return this.http.post<any>('/PR.php', data);
+    return this.http.post<any>('/api/PR.php', data);
   }
 
   deleteData(id: number): Observable<any> {
